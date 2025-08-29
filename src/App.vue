a8bfc5b894766d5bbf50c7db9ee9d836ae108e41
Odette Gallo <odette.gallo@hotmail.com>
2025-08-28 22:13:30 -0400

    mi primer commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup></script>
+
+<template>
+  <div class="container mt-5">
+    <h2>Usuarios Registrados</h2>
+    <TablaUsuarios />
+  </div>
+</template>
+
+<script>
+import TablaUsuarios from './components/TablaUsuarios.vue'
+
+export default {
+  components: {
+    TablaUsuarios,
+    FilaUsuario
+  }
+}
+</script>
+
+
+<style scoped></style>
---
+++ src/componentes/FilaUsuario.vue
@@ -0,0 +1,14 @@
+<template>
+  <tr>
+    <td>{{ usuario.nombre }}</td>
+    <td>{{ usuario.apellido }}</td>
+    <td>{{ usuario.fechaNacimiento }}</td>
+    <td>{{ usuario.edad }}</td>
+  </tr>
+</template>
+
+<script>
+export default {
+  props: ['usuario']
+}
+</script>
