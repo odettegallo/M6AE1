7fddaad23c3d4f2866df4b076d144dc866dca04a
Odette Gallo <odette.gallo@hotmail.com>
2025-09-02 21:52:38 -0400

    inicio

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,12 @@
-<script setup></script>
+<script setup>
+import TablaUsuarios from './componentes/TablaUsuarios.vue';
+</script>
 
 <template>
-  <div class="container mt-5">
-    <h2>Usuarios Registrados</h2>
-    <TablaUsuarios />
-  </div>
+  <TablaUsuarios />
 </template>
 
-<script>
-import TablaUsuarios from './components/TablaUsuarios.vue'
-import FilaUsuario from './components/FilaUsuarios.vue'
 
-export default {
-  components: {
-    TablaUsuarios,
-    FilaUsuario
-  }
-}
-</script>
 
 
 <style scoped></style>
